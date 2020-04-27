4627509bca565e9aaee8078bbdaf88fd0461d203
Joachim <jj@jj22.de>
2020-04-27 16:17:30 +0000

    jou

---
+++ Pair.java
@@ -0,0 +1,23 @@
+public class Pair<F,S> {
+	private F first;
+	private S second;
+	
+	public F getFirst(){
+		return first;
+	}
+
+	public S getSecond(){
+		return second;
+	}
+
+	public boolean equals(Object other){
+		if (!(other instanceof Pair)) return false;
+		return
+			F.equals(other.getFirst())
+			&& S.equals(other.getSecond());
+	}
+
+	public String toString(){
+		return first + ", " + second;
+	}
+}
