f5d0d4c2c2b58e452f5783d6bd271448990f8675
Joachim <jj@jj22.de>
2020-04-27 16:21:09 +0000

    jou

--- Pair.java
+++ Pair.java
@@ -2,6 +2,12 @@ public class Pair<F,S> {
 	private F first;
 	private S second;
 	
+	public Pair(F first, S second){
+		this.first = first;
+		this.second = second;
+	}
+
+
 	public F getFirst(){
 		return first;
 	}
