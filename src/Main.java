70f0ad61ee6d33efabb2c53c082731ca59c5838e
Joachim <jj@jj22.de>
2020-05-07 14:01:50 +0000

    asd

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
 	public static void main(String[] args) {
 		Pair a = new Pair(12,"Gakko");
+		Pair b = new Pair(12,"Gakko");
+		System.out.println(a.equals(b));
 	}
 }
--- src/Pair.java
+++ src/Pair.java
@@ -1,30 +1,65 @@
-public class Pair<F,S> {
-	private F first;
-	private S second;
+import java.util.Objects;
+
+public class Pair<F,S> implements Comparable<Pair> {
+	/*
+	zu Aufgabe 1.1:
+	alle Objektvariablen müssen final sein.
 	
-	public Pair(F first, S second){
+	Aufgabe 1.2:
+	Es ist sinnvoll, immutables zu verwenden, da man sich damit Null-Checks bzw. validierung der Variablen spart.
+	
+	Aufgabe 1.3:
+	Es ist schwer zu garantieren, dass generische Klassen komplett immutable sind, da man sie mit Objekten "füttern" kann, die nicht immutable sind.
+	Damit ist nur die Objektreferenz unverändert.
+	*/
+	private final F first;
+	private final S second;
+
+
+	public Pair(F first, S second) {
 		this.first = first;
 		this.second = second;
 	}
 
 
-	public F getFirst(){
-		return first;
+	@Override
+	public int hashCode() {
+		return Objects.hash(first, second);
 	}
 
-	public S getSecond(){
-		return second;
+
+	@Override
+	public int compareTo(T arg0) {
+		
+		return 0;
 	}
 
-	public boolean equals(Object other){
-		if (!(other instanceof Pair)) return false;
-		Pair otherPair = (Pair) other;
+
+	@Override
+	public boolean equals(Object otherObject) {
+		if (otherObject == this) return true;
+		if (!(otherObject instanceof Pair)) return false;
+		
+		Pair<?,?> otherPair = (Pair<?,?>) otherObject;
 		return
-			first.equals(otherPair.getFirst())
-			&& second.equals(otherPair.getSecond());
+			Objects.equals(first, otherPair.first)
+			&& Objects.equals(second, otherPair.second);
 	}
 
-	public String toString(){
-		return first + ", " + second;
+
+	@Override
+	public String toString() {
+		return "Pair("+ first +","+ second +")";
 	}
+
+
+	public F getFirst() {
+		return this.first;
+	}
+
+
+	public S getSecond() {
+		return this.second;
+	}
+
 }
