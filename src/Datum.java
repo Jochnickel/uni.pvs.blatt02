7020ea87353415cd1a52b796058a1e97fcfc90fa
Joachim <jj@jj22.de>
2020-05-07 21:05:42 +0000

    asd

---
+++ src/Datum.java
@@ -0,0 +1,15 @@
+import java.time.LocalDate;
+
+public class Datum {
+	public static void main(String[] args) {
+
+		System.out.println(LocalDate.now());
+		System.out.println("vv ---------------------------- vv");
+		System.out.println(LocalDate.now().minusWeeks(4));
+		System.out.println(LocalDate.now().minusMonths(1));
+		System.out.println("^^ Die Daten unterscheiden sich ^^");
+		System.out.println(LocalDate.now().minusWeeks(4).getDayOfWeek());
+		System.out.println(LocalDate.now().minusMonths(1).getDayOfWeek());
+
+	}
+}
--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,11 @@ import java.util.Iterator;
 import java.util.Stack;
 
 public class Main {
+	// ich fände es unnötig, für jede Teilaufgabe eine neue Methode zu schreiben (wird dadurch nicht übersichtlicher). also alles main()
 	public static void main(String[] args) {
 		//Aufgabe 2.2
 		Stack<Pair<String,Integer>> weDontNeedNoEducation = new Stack<Pair<String,Integer>>();
-		weDontNeedNoEducation.add(new Pair<String,Integer>("Informatik",1));
+		weDontNeedNoEducation.add(new Pair<String,Integer>("Informatik",1)); // doppelt
 		weDontNeedNoEducation.add(new Pair<String,Integer>("Deutsch",5));
 		weDontNeedNoEducation.add(new Pair<String,Integer>("Englisch",5));
 		weDontNeedNoEducation.add(new Pair<String,Integer>("Kunst",5));
@@ -15,27 +16,20 @@ public class Main {
 		weDontNeedNoEducation.add(new Pair<String,Integer>("Mathe",1));
 		weDontNeedNoEducation.add(new Pair<String,Integer>("Mathe",2));
 		weDontNeedNoEducation.add(new Pair<String,Integer>("Sport",6));
-		weDontNeedNoEducation.add(new Pair<String,Integer>("Informatik",1));
+		weDontNeedNoEducation.add(new Pair<String,Integer>("Informatik",1)); // doppelt
 		weDontNeedNoEducation.add(new Pair<String,Integer>("Informatik",3));
 		weDontNeedNoEducation.add(new Pair<String,Integer>("Sport",5));
 		Collections.sort(weDontNeedNoEducation);
 		weDontNeedNoEducation.forEach(grade -> System.out.println(grade));
+		System.out.println();
 
 		//Aufgabe 2.3
 		// HashSet<Pair<String,Integer>> hs1 = new HashSet<Pair<String,Integer>>(weDontNeedNoEducation);
 		HashSet<Pair<String,Integer>> hashBrowns = new HashSet<Pair<String,Integer>>();
 		
-		//1
 		for (Iterator<Pair<String,Integer>> iter = weDontNeedNoEducation.iterator(); iter.hasNext();){
 			hashBrowns.add(iter.next());
 		}
-		//2
-		for (Iterator<Pair<String,Integer>> iter = weDontNeedNoEducation.iterator(); iter.hasNext(); hashBrowns.add(iter.next()) );
-		//3
-		{
-			Iterator<Pair<String,Integer>> iter = weDontNeedNoEducation.iterator();
-			while(iter.hasNext()) hashBrowns.add(iter.next());
-		}
-		
+		hashBrowns.forEach(grade -> System.out.println(grade));
 	}
 }
